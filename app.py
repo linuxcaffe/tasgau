a3911eb164beb37bcfde696648d5f806892353cc
Alex Carney <alcarneyme@gmail.com>
2025-12-30 19:29:03 +0000

    Extract css, refactor cli exection, put projects into separate column

--- app.py
+++ app.py
@@ -5,11 +5,70 @@ import shlex
 import subprocess
 import sys
 from datetime import datetime
+from logging.config import dictConfig
 
 from flask import Flask, render_template, request, flash, redirect, url_for
 
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
 app = Flask(__name__)
 app.secret_key="insecure secret key that needs to be changed"
+
+logger = app.logger
+
+class TaskCommand:
+    """Represents a command to be run."""
+
+    def __init__(
+        self,
+        command: str,
+        filter: list[str] | None = None,
+        mods: list[str] | None = None
+    ):
+        self.command = command
+        """The actual command name, e.g 'add' or 'modify'"""
+
+        self.filter = filter
+        """The filter(s) to apply to a command."""
+
+        self.mods = mods
+        """The mods to apply to a command."""
+
+    def run(self):
+        """Run the specified command."""
+
+        args = [
+            "task",
+            *(self.filter or []),
+            self.command,
+            *(self.mods or []),
+        ]
+
+        logger.info(f"Running command: %r", args)
+        result = subprocess.run(args, capture_output=True)
+        if result.returncode != 0:
+            logger.error(
+                "Command exited with code %r\n%s",
+                result.returncode,
+                result.stderr.decode("utf-8"),
+            )
+            raise RuntimeError(f'task command exited with code {result.returncode}')
+
+        return result.stdout
 
 def relative_date(dt):
     """Format a datetime as a relative date string."""
@@ -17,7 +76,7 @@ def relative_date(dt):
     now = datetime.now()
     total_seconds = (now - dt).total_seconds()
 
-    if total_seconds < 86400: 
+    if total_seconds < 86400:
         if total_seconds < 60:
             return f"{int(total_seconds)}s ago"
         elif total_seconds < 3600:
@@ -58,13 +117,8 @@ def parse_tasks(data: bytes):
 
 @app.route('/')
 def index():
-    result = subprocess.run([
-        "task", "export", "next"
-    ], capture_output=True)
-    if result.returncode != 0:
-        raise ValueError('x')
-
-    items = parse_tasks(result.stdout)
+    command = TaskCommand("export", mods=["next"])
+    items = parse_tasks(command.run())
     return render_template("index.html", data=items, relative_date=relative_date, title="Next Tasks")
 
 
@@ -78,38 +132,26 @@ def tasks_complete():
     """Mark the given tasks as complete."""
     task_ids = [int(t) for t in request.form.getlist("selected_task_ids")]
 
-    result = subprocess.run([
-        "task", ",".join(str(t) for t in task_ids), "done"
-    ], capture_output=True)
-    if result.returncode != 0:
-        raise ValueError('x')
+    command = TaskCommand("done", filter=[",".join(str(t) for t in task_ids)])
+    result = command.run()
+    flash(result.decode("utf-8"))
 
-    flash(result.stdout.decode("utf-8"))
-
-    result = subprocess.run([
-        "task", "export", "next"
-    ], capture_output=True)
-    if result.returncode != 0:
-        raise ValueError('x')
-
-    items = parse_tasks(result.stdout)
+    command = TaskCommand("export", mods=["next"])
+    items = parse_tasks(command.run())
     return render_template("index.html", data=items, relative_date=relative_date, title="Next Tasks")
-
 
 
 @app.route('/tasks/add', methods=['POST'])
 def add_task():
     task = request.form.get('task')
-    result = subprocess.run([
-        "task", "add", *shlex.split(task)
-    ], capture_output=True)
-    if result.returncode != 0:
-        raise ValueError('x')
 
-    flash(result.stdout.decode('utf-8'))
+    command = TaskCommand("add", mods=shlex.split(task))
+    result = command.run()
+    flash(result.decode('utf-8'))
+
     return redirect(url_for('index'))
 
 
-
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format="[%(levelName)s]: %(message)s")
     app.run(debug=True)
